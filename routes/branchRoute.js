c0750a8d74eefc1568af410fb85cd8586916d1b3
Michael <mchljomen@example.com>
2026-05-08 23:43:00 +0800

    Fix my branchroute

--- routes/branchRoute.js
+++ routes/branchRoute.js
@@ -1,24 +1,36 @@
 import express from "express";
+
 import {
   getAllBranches,
   createBranch,
   updateBranch,
-  deleteBranch,          
-  restoreBranch,         
-  permanentDeleteBranch, 
-  getDeletedBranches,    
+  deleteBranch,
+  restoreBranch,
+  permanentDeleteBranch,
+  getDeletedBranches,
 } from "../controllers/branchController.js";
 
 import adminAuth from "../middleware/adminAuth.js";
 
 const branchRouter = express.Router();
 
-branchRouter.get("/list", adminAuth, getAllBranches);
+
+branchRouter.get("/list", getAllBranches);
+
 branchRouter.post("/add", adminAuth, createBranch);
+
 branchRouter.put("/update/:id", adminAuth, updateBranch);
+
 branchRouter.delete("/delete/:id", adminAuth, deleteBranch);
+
 branchRouter.post("/restore/:id", adminAuth, restoreBranch);
-branchRouter.delete("/permanent-delete/:id", adminAuth, permanentDeleteBranch);
+
+branchRouter.delete(
+  "/permanent-delete/:id",
+  adminAuth,
+  permanentDeleteBranch
+);
+
 branchRouter.get("/trash", adminAuth, getDeletedBranches);
 
 export default branchRouter;
